0e8298c7228f6c73d0fb1da383692686f0bf17c8
Mykyta Bondarenko <47217788+kensinghton@users.noreply.github.com>
2019-07-03 22:07:33 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,14 @@
- 1 #!/bin/bash
- 2 DIRNAME=log
- 3 F1=$(ls $DIRNAME|head -1)
- 4 F2=${F1##*/}
- 5 F3=${F2//[0-9]/}
- 6 echo $F3
- 7 FILENUM=$(ls -1 $DIRNAME | wc -l)
- 8 TRUENUM=0
- 9 while [[ $TRUENUM = '2' ]]; do
-10  for i in {1..$FILENUM}; do
-11         if [ `$(cat $F3$i)` == 'true' ; ]; then
-12           TRUENUM=$TRUENUM+1
-13           echo $TRUENUM
-14         fi
-15  done
-16 done
-17
-18 echo "$TRUENUM is done"
+#!/bin/bash
+DIRNAME=log
+F1=$(ls $DIRNAME|head -1)
+F2=${F1//[0-9]/}
+FILENUM=$(ls -1 $DIRNAME | wc -l)
+TRUENUM=0
+for i in $(seq 1 $FILENUM); do
+        if [ $(cat ./$DIRNAME/$F2$i) = 'true' ]; then
+          TRUENUM=$(($TRUENUM+1))
+                if [ $TRUENUM = 2 ]; then break 2 
+                fi
+         fi
+done 
+echo "$i was final"
