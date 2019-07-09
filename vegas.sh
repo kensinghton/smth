ccfd7745e3de55399edb663b2b7d6ef3595c0b3e
Mykyta Bondarenko <47217788+kensinghton@users.noreply.github.com>
2019-07-09 20:58:27 +0300

    Create vegas.sh

---
+++ vegas.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+cash=100
+dice_check='^[1-6]+$'
+cheater=''
+message=''
+clear
+echo "Welcome to fabulous Vegas.sh!\nLets play Dice!\nYou got $cash$\nEnter your name:"
+read player
+while : 
+do 
+        if [ "$cash" -le "0" ]; 
+        then echo "$player, you got no money, get out of here!\nYou got $cash$."; 
+                break 2 
+        else
+                clear
+                echo "$message\n"
+                echo "You got $cash$.\n $player, please type number between 1 and 6,\n or type 'quit' to leave, or 'add' $
+                read dice
+                roll=$(shuf -i 1-6 -n 1)
+                case $dice in
+                        [1-6])  if [ "$dice" -eq "$roll" ] ; 
+                                then message="Right! How do you do that, $player?\n$cheater"; cash=$((cash+20));
+                                else message="Wrong! My dice showed $roll, $player!\n$cheater"; cash=$((cash-10)); 
+                                fi ;;
+                        quit) break 2 ;;
+                        add) message="$player, you are cheater!"; cheater='By the way, you are cheating.'; cash=$((cash+1$
+                        rem) message="Excuse me, what???"; cheater=''; cash=$((cash-1000)) ;;
+                        *) message="Try again, $player\n" ;;       ##Abandon the loop.
+                esac    
+        fi
+done
