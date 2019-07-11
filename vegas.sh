7df63577809d57d1e075d44dc866e9db050cb3f9
Mykyta Bondarenko <47217788+kensinghton@users.noreply.github.com>
2019-07-11 20:03:59 +0300

    Update vegas.sh

--- vegas.sh
+++ vegas.sh
@@ -14,7 +14,7 @@ do
         else
                 clear
                 echo "$message\n"
-                echo "You got $cash$.\n $player, please type number between 1 and 6,\n or type 'quit' to leave, or 'add' $
+                echo "You got $cash$.\n $player, please type number between 1 and 6,\n or type 'quit' to leave, or 'add' or 'rem' for... something:"
                 read dice
                 roll=$(shuf -i 1-6 -n 1)
                 case $dice in
@@ -23,9 +23,10 @@ do
                                 else message="Wrong! My dice showed $roll, $player!\n$cheater"; cash=$((cash-10)); 
                                 fi ;;
                         quit) break 2 ;;
-                        add) message="$player, you are cheater!"; cheater='By the way, you are cheating.'; cash=$((cash+1$
+                        add) message="$player, you are cheater!"; cheater='By the way, you are cheating.'; cash=$((cash+1500)) ;;
                         rem) message="Excuse me, what???"; cheater=''; cash=$((cash-1000)) ;;
-                        *) message="Try again, $player\n" ;;       ##Abandon the loop.
+                        *) message="Try again, $player\n" ;;
                 esac    
         fi
 done
+
